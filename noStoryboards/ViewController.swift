a9630a0e6e21eb20bdb3bf4cc95f4be8ae59f228
shravansukumar <shravan.sukumar@gmail.com>
2017-03-08 18:09:32 +0530

    Added navigation controller

---
+++ noStoryboards/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  noStoryboards
+//
+//  Created by Shravan Sukumar on 08/03/17.
+//  Copyright © 2017 shravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigation()
+        self.view.backgroundColor = UIColor.white
+    }
+
+    func setupNavigation() {
+        navigationItem.title = "2017 Holi- Rang Munch"
+    }
+    
+   
+
+}
+
