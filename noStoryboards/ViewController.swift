073046dc5bf3522df1b6fba2e142040b2b5a9611
shravansukumar <shravan.sukumar@gmail.com>
2017-03-10 00:53:16 +0530

    Summary view complete

--- noStoryboards/ViewController.swift
+++ noStoryboards/ViewController.swift
@@ -39,24 +39,87 @@ class ViewController: UIViewController {
     }
     
     func setupSummaryView() {
-         summaryView = UIView(frame: CGRect(x: 0.0, y: Double(homeScreenImageView.frame.maxY), width: Double(self.view.frame.width), height: Double(self.view.frame.height * 0.2)))
+         summaryView = UIView(frame: CGRect(x: 0.0, y: Double(homeScreenImageView.frame.maxY), width: Double(self.view.frame.width), height: Double(self.view.frame.height * 0.160)))
         summaryView.backgroundColor = custonWhiteColor
         self.view.addSubview(summaryView)
         
         let dateLabel = UILabel()
+        let titleLabel = UILabel()
+        let locationLabel = UILabel()
+        let clockLabel = UILabel()
+        
+        let locationImage = #imageLiteral(resourceName: "location_image")
+        let locationImageView = UIImageView(image: locationImage)
+        
+        let clockImage = #imageLiteral(resourceName: "clock_image")
+        let clockImageView = UIImageView(image: clockImage)
+        
         dateLabel.textAlignment = .center
         dateLabel.numberOfLines = 0
         dateLabel.text = "MON 13th MAR"
+        
+        titleLabel.textAlignment = .left
+        titleLabel.numberOfLines = 0
+        titleLabel.text = "2017 Holi - Rang Munch"
+        
+        locationLabel.textAlignment = .left
+        locationLabel.numberOfLines = 0
+        locationLabel.text = "at Pebble- The Jungle Lounge, Bengaluru"
+        locationLabel.adjustsFontSizeToFitWidth = true
+        locationLabel.font = locationLabel.font.withSize(12)
+        
+        clockLabel.textAlignment = .left
+        clockLabel.text = "10 AM"
+        
+        
+        summaryView.addSubview(titleLabel)
         summaryView.addSubview(dateLabel)
+        summaryView.addSubview(locationImageView)
+        summaryView.addSubview(locationLabel)
+        summaryView.addSubview(clockImageView)
+        summaryView.addSubview(clockLabel)
         
-        let horizontalConstraint = NSLayoutConstraint(item: dateLabel, attribute: .leading, relatedBy: .equal, toItem: summaryView, attribute: .leadingMargin, multiplier: 1.0, constant: 5.0)
-        let verticalConstraint = NSLayoutConstraint(item: dateLabel, attribute: .top, relatedBy: .equal, toItem: summaryView,
-            attribute: .topMargin, multiplier: 1.0, constant: 0.0)
-        let heightConstraint = NSLayoutConstraint(item: dateLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 80.0)
-        let widthConstraint = NSLayoutConstraint(item: dateLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 40.0)
-        
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
-        summaryView.addConstraints([horizontalConstraint, verticalConstraint, heightConstraint, widthConstraint])
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        clockImageView.translatesAutoresizingMaskIntoConstraints = false
+        clockLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        // MARK: - Date Label Constraints
+        let dateLabelHorizontalConstraint = NSLayoutConstraint(item: dateLabel, attribute: .leading, relatedBy: .equal, toItem: summaryView, attribute: .leadingMargin, multiplier: 1.0, constant: 5.0)
+        let dateLabelVerticalConstraint = NSLayoutConstraint(item: dateLabel, attribute: .top, relatedBy: .equal, toItem: summaryView,
+            attribute: .topMargin, multiplier: 1.0, constant: 0.0)
+        let dateLabelHeightConstraint = NSLayoutConstraint(item: dateLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 80.0)
+        let dateLabelWidthConstraint = NSLayoutConstraint(item: dateLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 45.0)
+        
+        // MARK: - Title Label constraints
+        let titleLabelHorizontalConstraint = NSLayoutConstraint(item: summaryView, attribute: .trailingMargin, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1.0, constant: 90.0)
+        let titleLabelVerticalConstraint = NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: summaryView, attribute: .top, multiplier: 1.0, constant: 15.0)
+        
+        // MARK: - Location Image View constraints
+        let locationImageViewHeightConstraint = NSLayoutConstraint(item: locationImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 20.0)
+        let locationImageViewWidthConstraint = NSLayoutConstraint(item: locationImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 20.0)
+        let locationImageViewTopConstraint = NSLayoutConstraint(item: locationImageView, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1.0, constant: 8)
+        let locationImageViewLeadingConstraint = NSLayoutConstraint(item: locationImageView, attribute: .leading, relatedBy: .equal, toItem: titleLabel, attribute: .leading, multiplier: 1.0, constant: 0)
+        
+        // MARK: - Location Label constraints
+        let locationLabelLeadingConstraint = NSLayoutConstraint(item: locationLabel, attribute: .leading, relatedBy: .equal, toItem: locationImageView, attribute: .trailing, multiplier: 1.0, constant: 2.0)
+        let locationLabelCenterConstraint = NSLayoutConstraint(item: locationLabel, attribute: .centerY, relatedBy: .equal, toItem: locationImageView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        
+        // MARK: - Clock Image View Constraints
+        let clockImageViewHeightConstraint = NSLayoutConstraint(item: clockImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 20.0)
+        let clockImageViewWidthConstraint = NSLayoutConstraint(item: clockImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 20.0)
+        let clockImageViewLeadingConstraint = NSLayoutConstraint(item: clockImageView, attribute: .leading, relatedBy: .equal, toItem: locationImageView, attribute: .leading, multiplier: 1.0, constant: 0)
+        let clockImageViewTopConstraint = NSLayoutConstraint(item: clockImageView, attribute: .top, relatedBy: .equal, toItem: locationImageView, attribute: .bottom, multiplier: 1.0, constant: 8)
+        
+        // MARK: - Clock Label constraints
+        let clockLabelLeadingConstraint =  NSLayoutConstraint(item: clockLabel, attribute: .leading, relatedBy: .equal, toItem: clockImageView, attribute: .trailing, multiplier: 1.0, constant: 2.0)
+        let clockLabelCenterConstraint = NSLayoutConstraint(item: clockLabel, attribute: .centerY, relatedBy: .equal, toItem: clockImageView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        
+        let constraintsArray = [dateLabelHorizontalConstraint, dateLabelVerticalConstraint, dateLabelHeightConstraint, dateLabelWidthConstraint, titleLabelHorizontalConstraint, titleLabelVerticalConstraint, locationImageViewHeightConstraint, locationImageViewWidthConstraint, locationImageViewTopConstraint, locationImageViewLeadingConstraint, locationLabelLeadingConstraint, locationLabelCenterConstraint, clockImageViewWidthConstraint, clockImageViewHeightConstraint, clockImageViewTopConstraint, clockImageViewLeadingConstraint, clockLabelLeadingConstraint, clockLabelCenterConstraint]
+
+        summaryView.addConstraints(constraintsArray)
     }
 }
 
