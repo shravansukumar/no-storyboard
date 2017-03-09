510034c6b89ceef40515b24fc21ca9c9fdd9875a
shravansukumar <shravan.sukumar@gmail.com>
2017-03-09 12:25:15 +0530

    Added home page image

--- noStoryboards/ViewController.swift
+++ noStoryboards/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Variables
+    var homeScreenImageView: UIImageView! = nil
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupNavigation()
+        setupImageView()
         self.view.backgroundColor = UIColor.white
     }
-
+    
+    // MARK: - Methods
     func setupNavigation() {
-        navigationItem.title = "2017 Holi- Rang Munch"
+        navigationItem.title = "2017 Holi - Rang Munch"
     }
     
-   
-
+    func setupImageView() {
+        let navigationBarHeight = Double((self.navigationController?.navigationBar.frame.height)!)
+        let homeScreenImage = UIImage(named: "homepage_image")
+        homeScreenImageView = UIImageView(image: homeScreenImage!)
+        homeScreenImageView.frame = CGRect(x: 0.0, y: navigationBarHeight + 10.0, width: (Double(self.view.frame.width)), height: Double(self.view.frame.height * 0.2))
+        view.addSubview(homeScreenImageView)
+    }
 }
 
