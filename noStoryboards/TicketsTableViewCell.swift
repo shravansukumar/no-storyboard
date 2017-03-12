cfba32f4bde2bcb6d0d38d260772e26e0d368a27
shravansukumar <shravan.sukumar@gmail.com>
2017-03-12 21:36:06 +0530

    Added ticket count view

--- noStoryboards/TicketsTableViewCell.swift
+++ noStoryboards/TicketsTableViewCell.swift
@@ -15,6 +15,11 @@ class TicketsTableViewCell: UITableViewCell {
     var priceLabel = UILabel()
     var lastDateLabel = UILabel()
     var bottomSeperatorView = UIView()
+    var ticketsView = UIView()
+    var minusButton = UIButton()
+    var plusButton = UIButton()
+    var countLabel = UILabel()
+    var defaultCountLabelText = 0
     
     // MARK: - Lifecycle
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -24,17 +29,65 @@ class TicketsTableViewCell: UITableViewCell {
         self.contentView.addSubview(priceLabel)
         self.contentView.addSubview(lastDateLabel)
         self.contentView.addSubview(bottomSeperatorView)
+        self.contentView.addSubview(ticketsView)
+        self.ticketsView.addSubview(minusButton)
+        self.ticketsView.addSubview(countLabel)
+        self.ticketsView.addSubview(plusButton)
         
+       // ticketsView.backgroundColor = UIColor.black
         bottomSeperatorView.backgroundColor = UIColor.lightGray
         
         ticketTypeLabel.translatesAutoresizingMaskIntoConstraints = false
         priceLabel.translatesAutoresizingMaskIntoConstraints = false
         lastDateLabel.translatesAutoresizingMaskIntoConstraints = false
         bottomSeperatorView.translatesAutoresizingMaskIntoConstraints = false
+        ticketsView.translatesAutoresizingMaskIntoConstraints = false
+        minusButton.translatesAutoresizingMaskIntoConstraints = false
+        countLabel.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupTicketsViewElements()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    func setupTicketsViewElements() {
+        minusButton.setImage(#imageLiteral(resourceName: "minus_button"), for: .normal)
+        minusButton.addTarget(self, action: #selector(minusButtonTapped), for: .touchUpInside)
+        
+        plusButton.setImage(#imageLiteral(resourceName: "plus_button"), for: .normal)
+        plusButton.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
+        
+        countLabel.text = String(defaultCountLabelText)
+        countLabel.font = countLabel.font.withSize(12)
+        
+        if countLabel.text == String(defaultCountLabelText) {
+            minusButton.isEnabled = false
+        }
+    }
+    
+    
+    func minusButtonTapped() {
+        print("Minus Button Tapped")
+        var ticketCount = Int(countLabel.text!)
+        if ticketCount == 0 {
+            minusButton.isEnabled = false
+        } else {
+            ticketCount = ticketCount! - 1
+            countLabel.text = String(ticketCount!)
+        }
+    }
+    
+    func plusButtonTapped() {
+        print("Plus Button Tapped")
+        if minusButton.isEnabled == false {
+            minusButton.isEnabled = true
+        }
+        var ticketCount = Int(countLabel.text!)
+        ticketCount = ticketCount! + 1
+        countLabel.text = String(ticketCount!)
     }
     
     override func layoutSubviews() {
@@ -58,7 +111,36 @@ class TicketsTableViewCell: UITableViewCell {
         let bottomViewBottomConstraint = NSLayoutConstraint(item: self.contentView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomSeperatorView, attribute: .bottom, multiplier: 1, constant: 0)
         let bottomViewHeightConstraint = NSLayoutConstraint(item: bottomSeperatorView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 1)
         
-        let constraintsArray = [ticketTypeLabelLeadingConstraint, ticketTypeLabelTopConstraint, priceLabelLeadingConstraint, priceLabelTopConstraint, lastDateLabelBaseLineConstraint, lastDateLabelLeadingConstraint, bottomViewBottomConstraint, bottomViewHeightConstraint, bottomViewLeadingConstraint, bottomViewTrailingConstraint]
+        // MARK: - Tickets View Constraints
+        let ticketsViewWidthConstraint = NSLayoutConstraint(item: ticketsView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 70)
+        let ticketsViewHeightConstraint = NSLayoutConstraint(item: ticketsView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40)
+        let ticketsViewCenterConstraint = NSLayoutConstraint(item: ticketsView, attribute: .centerY, relatedBy: .equal, toItem: self.contentView, attribute: .centerY, multiplier: 1, constant: 0)
+        let ticketsViewTrailingConstraint = NSLayoutConstraint(item: self.contentView, attribute: .trailing, relatedBy: .equal, toItem: ticketsView, attribute: .trailing, multiplier: 1, constant: 25)
+        
+        // MARK: - Minus Button Constraints
+        let minusButtonWidthConstraint = NSLayoutConstraint(item: minusButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20)
+        let minusButtonHeightConstraint = NSLayoutConstraint(item: minusButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20)
+        let minusButtonLeadingConstraint = NSLayoutConstraint(item: minusButton, attribute: .leading, relatedBy: .equal, toItem: ticketsView, attribute: .leading, multiplier: 1, constant: 5)
+        let minusButtonCenterConstraint = NSLayoutConstraint(item: minusButton, attribute: .centerY, relatedBy: .equal, toItem: ticketsView, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        // MARK: - Count Label Constraints
+        let countLabelLeadingConstraints = NSLayoutConstraint(item: countLabel, attribute: .leading, relatedBy: .equal, toItem: minusButton, attribute: .leading, multiplier: 1, constant: 30)
+        let countLabelCenterConstraint = NSLayoutConstraint(item: countLabel, attribute: .centerY, relatedBy: .equal, toItem: ticketsView, attribute: .centerY, multiplier: 1, constant: 0)
+        let countLabelWidthConstraint = NSLayoutConstraint(item: countLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 10)
+    
+        
+        // MARK: - Plus Button Constraints
+        let plusButtonWidthConstraint = NSLayoutConstraint(item: plusButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20)
+        let plusButtonHeightConstraint = NSLayoutConstraint(item: plusButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20)
+        let plusButtonLeadingConstraints = NSLayoutConstraint(item: plusButton, attribute: .leading, relatedBy: .equal, toItem: countLabel, attribute: .trailing, multiplier: 1, constant: 10)
+        let plusButtonCenterConstraint = NSLayoutConstraint(item: plusButton, attribute: .centerY, relatedBy: .equal, toItem: ticketsView, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        // Tickets View Constraints
+        let ticketViewConstraintArray = [minusButtonWidthConstraint, minusButtonHeightConstraint, minusButtonCenterConstraint, minusButtonLeadingConstraint, countLabelLeadingConstraints, countLabelCenterConstraint, countLabelWidthConstraint, plusButtonWidthConstraint, plusButtonCenterConstraint, plusButtonHeightConstraint, plusButtonLeadingConstraints]
+        self.ticketsView.addConstraints(ticketViewConstraintArray)
+        
+        // Content View Constraint
+        let constraintsArray = [ticketTypeLabelLeadingConstraint, ticketTypeLabelTopConstraint, priceLabelLeadingConstraint, priceLabelTopConstraint, lastDateLabelBaseLineConstraint, lastDateLabelLeadingConstraint, bottomViewBottomConstraint, bottomViewHeightConstraint, bottomViewLeadingConstraint, bottomViewTrailingConstraint, ticketsViewWidthConstraint, ticketsViewCenterConstraint, ticketsViewHeightConstraint, ticketsViewTrailingConstraint]
         self.contentView.addConstraints(constraintsArray)
     }
 
